977ce78af368de3447a90227e6c68f13556cc39e
姚力晓 <634355316@qq.com>
2025-12-22 09:51:01 +0800

    feat: implement unique paths with obstacles

--- dp/unique_paths.go
+++ dp/unique_paths.go
@@ -40,3 +40,52 @@ func UniquePathsOptimized(m, n int) int {
 
 	return dp[n-1]
 }
+
+func UniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m := len(obstacleGrid)
+	n := len(obstacleGrid[0])
+
+	// 1. 只要起点是石头，或者终点是石头，直接 0 种方法，不用算了
+	if obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1 {
+		return 0
+	}
+
+	// 申请一维数组
+	dp := make([]int, n)
+
+	// 2. 初始化第一行 (Base Case)
+	// 逻辑：起点肯定是 1。
+	// 只要碰到一个石头，后面全都是 0（因为只能向右走，被挡住了就过不去了）
+	for j := 0; j < n; j++ {
+		if obstacleGrid[0][j] == 1 {
+			dp[j] = 0 // 其实默认就是0，这就相当于 break 的效果
+			// 在这一行，石头后面的格子因为 dp 默认是 0，所以不需要额外操作
+			// 但为了逻辑严谨，一旦碰到石头，这一行后面的都不用设为 1 了
+			break
+		} else {
+			dp[j] = 1
+		}
+	}
+
+	// 3. 从第二行开始遍历
+	for i := 1; i < m; i++ {
+		for j := 0; j < n; j++ {
+			// 情况 A：当前位置是石头
+			if obstacleGrid[i][j] == 1 {
+				dp[j] = 0 // 此路不通，设为 0
+				continue  // 继续看下一个格子，千万不能 break！
+			}
+
+			// 情况 B：当前位置是空地
+			// dp[j] 目前存的是“上一行”的值（从上面下来的路）
+			// 我们要加上“左边”的值（从左边过来的路）
+			if j > 0 {
+				dp[j] = dp[j] + dp[j-1]
+			}
+			// 如果 j == 0 (第一列)，dp[j] 就保持原样（只接受从上面下来的路径）
+			// 但如果第一列上面那个位置本来就是 0 (被堵住了)，那它自然也是 0，逻辑自洽。
+		}
+	}
+
+	return dp[n-1]
+}
